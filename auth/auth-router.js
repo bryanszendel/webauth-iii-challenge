9f77f7a6725253a26a6f3aa6b81c503e1e4906af
Bryan Szendel <bryan.szendel@gmail.com>
2019-09-18 14:04:07 -0600

    /api/auth/register registers user, saved to db, and returns a token

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -3,12 +3,34 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const Users = require('../users/users-model.js');
-// const secrets = require('../config/secrets.js')
+const secrets = require('../config/secrets.js')
 
 // ENDPOINTS
 
 router.post('/register', (req, res) => {
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10)
+  user.password = hash;
 
+  Users.add(user)
+    .then(saved => {
+      const token = generateToken(user)
+      res.status(201).json({saved, token})
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    })
 })
+
+function generateToken(user) {
+  const payload = {
+    username: user.username
+  }
+  const options = {
+    expiresIn: '1d'
+  }
+
+  return jwt.sign(payload, secrets.jwtSecret, options)
+}
 
 module.exports = router;
